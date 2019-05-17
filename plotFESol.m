f20d95dea5cc5f427e464ba05bb79c70fac60cbb
samchris1995 <schristen@g.ucla.edu>
2019-05-17 15:23:19 -0700

    plotFESol.m

---
+++ plotFESol.m
@@ -0,0 +1,20 @@
+function plotFESol(p,t,U)
+if size(p,1)~=length(p)
+p=p';
+end
+if size(U,1)~=length(U)
+U=U';
+end
+figure
+set(gca, 'Projection', 'perspective')
+caxis([0 1])
+daspect([1 1 2])
+view(70, 20)
+axis vis3d
+axis([-1 1 -1 1 -1 1])
+cla
+trisurf(t, p(:,1), p(:,2), U, 'EdgeColor', [0.5 0.5 0.5], 'FaceColor', 'interp');
+colorbar
+drawnow
+
+end
