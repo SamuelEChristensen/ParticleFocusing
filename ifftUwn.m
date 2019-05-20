e5a8afae05971175391c4d7c2c402b78c0cbb207
samchris1995 <schristen@g.ucla.edu>
2019-05-19 17:01:13 -0700

    made ifftUwn

---
+++ ifftUwn.m
@@ -0,0 +1,26 @@
+function U = ifftUwn(Uwn)
+
+maxWaveNum = length(Uwn);
+U1 = zeros(length(Uwn{1,1}), maxWaveNum);
+U2 = zeros(length(Uwn{1,1}), maxWaveNum);
+U3 = zeros(length(Uwn{1,1}), maxWaveNum);
+P = zeros(length(Uwn{1,4}), maxWaveNum);
+U = cell(maxWaveNum,4);
+for i = 1:maxWaveNum
+    U1(:,i) = Uwn{i,1};
+    U2(:,i) = Uwn{i,2};
+    U3(:,i) = Uwn{i,3};
+    P(:,i) = Uwn{i,4};
+end
+
+U1 = ifft(U1,maxWaveNum,2);
+U2 = ifft(U2,maxWaveNum,2);
+U3 = ifft(U3,maxWaveNum,2);
+P = ifft(P,maxWaveNum,2);
+
+for i = 1:maxWaveNum  
+    U(i,1) = {U1(:,i)};
+    U(i,2) = {U2(:,i)};
+    U(i,3) = {U3(:,i)};
+    U(i,4) = {P(:,i)};
+end
